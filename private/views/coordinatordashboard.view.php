b0ac907a55ac9f2a1eb4159946dbdfcfd1f39ec7
GaveeshaWickrama <gaveesha.wickrama2001@gmail.com>
2023-11-09 17:33:17 +0530

    Update coordinatordashboard.view.php

--- private/views/coordinatordashboard.view.php
+++ private/views/coordinatordashboard.view.php
@@ -1,16 +1,6 @@
-<!-- <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../public/styles/components.css">
-    <title>Home</title>
-</head>
-<body> -->
-<?php require '../private/views/components/header.view.php'; ?> 
+ <?php $this->view('includes/header')?>
 
     <p class="pclass"> coordinator</p>
-<!-- </body>
-</html> -->
 
-<?php require '../private/views/components/footer.view.php'; ?> 
+<?php $this->view('includes/footer'); ?>
+
