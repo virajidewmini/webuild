c989a92a837e7d019cf478fdce8f1500ac31d812
Thenuwara <pramukhapoo@gmail.com>
2023-11-09 23:53:12 +0530

    Added PM dashboard

---
+++ private/controllers/Pmdashboard.php
@@ -0,0 +1,11 @@
+<?php
+
+    //coordinator dashboard controller
+    class Pmdashboard extends Controller{
+        
+        public function index(){
+            $this->view('pmdashboard');
+        }
+
+    }
+?>
---
+++ private/views/pmdashboard.view.php
@@ -0,0 +1,10 @@
+<?php $this->view('includes/header')?>
+<style>
+main {
+  background-image:url("<?=ROOT?>/img/pm.jpg");
+}
+</style>
+
+
+<?php $this->view('includes/footer'); ?>
+
