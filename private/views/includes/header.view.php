c145f6a2b7e027e3839c0db2ca87934f1f75f2c4
GaveeshaWickrama <gaveesha.wickrama2001@gmail.com>
2023-11-09 17:45:23 +0530

    Update header.view.php

--- private/views/includes/header.view.php
+++ private/views/includes/header.view.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="<?=ROOT?>/css/style.css">
-    <title>Document</title>
+    <title><?= SITENAME ?></title>
 </head>
 <body>
     <div style="min-width: 350px;">
