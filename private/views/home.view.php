8c2579cab7214a01a1e4e7deace6334e65fafa17
HimashaRandika <136648121+HimashaRandika@users.noreply.github.com>
2023-11-10 20:25:57 +0530

    Update home.view.php

--- private/views/home.view.php
+++ private/views/home.view.php
@@ -7,7 +7,7 @@
 
   <!-- font awesome cdn link -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
-  <link rel="stylesheet" href="./style.css">
+  <link rel="stylesheet" href="./public/css/home.css">
 </head>
 <body>
 
