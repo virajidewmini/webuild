672f54aedd24f664018d59944b29006063edac02
GaveeshaWickrama <gaveesha.wickrama2001@gmail.com>
2023-11-11 11:36:58 +0530

    Userprofile controller and view created

---
+++ private/controllers/Userprofile.php
@@ -0,0 +1,11 @@
+<?php
+
+//userprofile controller
+    class Userprofile extends Controller{
+
+        public function index(){
+            $this->view('userprofile');
+        }
+
+    }
+?>
---
+++ private/views/userprofile.view.php
@@ -0,0 +1,8 @@
+<?php $this->view('includes/header')?>
+
+
+     <h1>Profile</h1>
+     
+
+<?php $this->view('includes/footer'); ?>
+
