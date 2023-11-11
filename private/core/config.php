c3ea42f6b4c6c74e9893cad53f031ca28eb96b00
Viraji Dewmini <virajidewmini@gmail.com>
2023-11-11 20:04:10 +0530

    Update config.php

--- private/core/config.php
+++ private/core/config.php
@@ -1,11 +1,5 @@
 <?php
-define('ROOT', 'http://localhost:8888/webuild/public');
-define('ASSETS', 'http://localhost:8888/webuild/public/assets');
-
-define('DBNAME', 'webuild');
-define('DBHOST', 'localhost');
-define('DBUSER', 'root');
-define('DBPASS', 'root');
-define('DBDRIVER', 'mysql');
+    define ('ROOT','http://localhost/webuild/public');
+    define('SITENAME', 'webuild');
 
 ?>
