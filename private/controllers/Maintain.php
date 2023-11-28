d3be57ab3e581d9538cea220a939a85eb98d13ac
HimashaRandika <136648121+HimashaRandika@users.noreply.github.com>
2023-11-29 00:23:44 +0530

    Storekeeper Maintain

---
+++ private/controllers/Maintain.php
@@ -0,0 +1,11 @@
+<?php
+
+    //Manager materials controller
+    class Maintain extends Controller{
+        
+        public function index(){
+            $this->view('storekeeperMaintain');
+        }
+
+    }
+?>
