90b8736014696dcc068494d1bc1378a68e50d9b9
HimashaRandika <136648121+HimashaRandika@users.noreply.github.com>
2023-11-28 15:42:49 +0530

    Create landing.view.php

---
+++ private/views/landing.view.php
@@ -0,0 +1,284 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Lands</title>
+
+  <!-- font awesome cdn link -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+  <link rel="stylesheet" href="./style.css">
+</head>
+<body>
+
+<!-- header section starts -->
+
+<section class="header">
+
+<!-- <a href="./home.php" class="logo">
+        <img src="./Photos/LOGO2.png" alt="" class="LOGO-img" width="50" hight="50">
+</a> -->
+
+<a href="./houses.php" class="logo">WEBUILD</a>
+<nav class="navbar">
+  <a href="./lands.php">Lands</a>
+  <a href="./houses.php">House</a>
+  <a href="./completed.php">Completed Projects</a>
+  <a href="./about.php">About Us</a>
+  <a href="./contact.php">Contact Us</a>
+</nav>
+
+
+
+
+<div id="menu-btn" class="fas fa-bars"></div>
+
+</section>
+
+<!-- header section ends -->
+
+<!-- home section starts -->
+<section class="home">
+  <div class="home-slide">
+    <div class="W">
+      <div class="slide" style="background: url(./Photos/741_house_1.jpg);">
+        <div class="content">
+          <span>purchase your dream place</span>
+          <h3>dream with webuild</h3>
+          <a href="#" class="btn">See More</a>       
+         </div>
+      </div>
+      <div class="slide" style="background: url(./Photos/1630000306A.jpg);">
+        <div class="content">
+          <span>purchase your dream place</span>
+          <h3>dream with webuild</h3>
+          <a href="#" class="btn">See More</a>        
+        </div>
+      </div>
+      <div class="slide" style="background: url(./Photos/hq720.jpg)">
+        <div class="content">
+          <span>purchase your dream place</span>
+          <h3>dream with webuild</h3>
+          <a href="#" class="btn">See More</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- home section ends -->
+
+<!-- filter section starts-->
+<div class="slide" style="background: url(./Photos/hq720.jpg)">
+        <div class="content">
+        <section class="booking">
+  <h1 class="heading-title"> Contact Us
+  <form action="book_form.php" method="post" class="book-form">
+    <div class="flex">
+      <div class="inputBox">
+        <span>looking for:</span>
+        <input type="text" placeholder="Lands" name="name">
+      </div>
+      <div class="inputBox">
+        <span>District:</span>
+        <input type="text" placeholder="All District" name="email">
+      </div>
+      <div class="inputBox">
+        <span>popular ciites:</span>
+        <input type="text" placeholder="Cities" name="phone">
+      </div>
+      <div class="inputBox">
+        <span>search:</span>
+        <input type="text" placeholder="Type here" name="address">
+      </div>
+    </div>
+    <input type="submit" value="submit" class="btn" name="send">
+  </form>  
+  </h1>
+</section>
+        </div>
+      </div>
+
+
+<!-- filter section ends-->
+
+
+
+<!-- service section  starts -->
+<section class="services">
+  <h1 class="heading-title">Our Houses</h1>
+  <div class="box-container">
+    <!-- <div class="box1">
+      <img src="images/icon-1.png" alt="">
+    </div>
+    <div class="box2">
+      <img src="./Photos/images.jpeg" alt="">
+    </div>
+    <div class="box3">
+      <img src="./Photos/741_house_1.jpg" alt="">
+    </div>
+    <div class="box4">
+      <img src="./Photos/00.jpg" alt="">
+    </div> -->
+  </div>
+</section>
+<!-- service section ends -->
+
+
+
+<!-- home packages section starts  -->
+<section class="home-packages">
+  <h1 class="heading-title">Our packages</h1>
+  <div class="box-container">
+    <div class="box">
+      <div class="image">
+        <img src="./Photos/images5.jpeg" alt="">
+      </div>
+      <div class="content">
+        <h3>Purch Your Lands</h3>
+        <p>jdnjndnnnn jmkkmk okmlkmmf kmmm</p>
+        <a href="#" class="btn">Visit</a>
+      </div>
+    </div>
+    <div class="box">
+      <div class="image">
+        <img src="./Photos/images5.jpeg" alt="">
+      </div>
+      <div class="content">
+        <h3>Purch Your Lands</h3>
+        <p>jdnjndnnnn jmkkmk okmlkmmf kmmm</p>
+        <a href="#" class="btn">Visit</a>
+      </div>
+    </div>
+    <div class="box">
+      <div class="image">
+        <img src="./Photos/images5.jpeg" alt="">
+      </div>
+      <div class="content">
+        <h3>Purch Your Lands</h3>
+        <p>jdnjndnnnn jmkkmk okmlkmmf kmmm</p>
+        <a href="#" class="btn">Visit</a>
+      </div>
+    </div>
+  </div>
+  <div class="load-more"><a href="#" class="btn">Load more</a></div>
+</section>
+<!-- home packages section ends  -->
+
+<!-- home offer section starts -->
+<section class="home-offer">
+  <div class="content">
+    <h3>upto 3% off</h3>
+    <p>jijojojioj jjkikjd jijijd ffrw l,lw wtftfwd ftfw rcf tft tftw guhj   </p>
+    <a href="#" class="btn">book now</a>
+  </div>
+</section>
+<!-- home offer section ends -->
+
+<!-- reviews section starts -->
+<section class="reviews">
+  <div class="reviews-slider">
+    <div class="w">
+      <div class="slide">
+        <div class="stars">
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+        </div>
+        <p>lorem huh huhuhs sjois ijiia ijis iji ijiw ijiw ijiw ijijw ijiw ijiw jiw oow</p>
+        <h3>N.M.Siriwardhane</h3>
+        <span>Doctor</span>
+      </div>
+      <div class="slide">
+        <div class="stars">
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+        </div>
+        <p>lorem huh huhuhs sjois ijiia ijis iji ijiw ijiw ijiw ijijw ijiw ijiw jiw oow</p>
+        <h3>N.M.Siriwardhane</h3>
+        <span>Doctor</span>
+      </div>
+      <div class="slide">
+        <div class="stars">
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+        </div>
+        <p>lorem huh huhuhs sjois ijiia ijis iji ijiw ijiw ijiw ijijw ijiw ijiw jiw oow</p>
+        <h3>N.M.Siriwardhane</h3>
+        <span>Doctor</span>
+      </div>
+      <div class="slide">
+        <div class="stars">
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+        </div>
+        <p>lorem huh huhuhs sjois ijiia ijis iji ijiw ijiw ijiw ijijw ijiw ijiw jiw oow</p>
+        <h3>N.M.Siriwardhane</h3>
+        <span>Doctor</span>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- reviews section ends -->
+
+
+
+
+
+
+
+
+
+<!-- footer section starts -->
+<section class="footer">
+  <div class="box-container">
+    <div class="box">
+      <h3>Quik links</h3>
+      <a href="./home.php"><i class="fas fa-angle-right"></i>Houses</a>
+      <a href="./completed.php"><i class="fas fa-angle-right"></i>Completed Projects</a>
+      <a href="./about.php"><i class="fas fa-angle-right"></i>About Us</a>
+      <a href="./contact.php"><i class="fas fa-angle-right"></i>Contact Us</a>
+    </div>
+
+    <div class="box">
+      <h3>Extra Links</h3>
+      <a href="./about.php"><i class="fas fa-angle-right"></i>About Us</a>
+      <a href="./contact.php"><i class="fas fa-angle-right"></i>Privacy Policy</a>
+    </div>
+
+    <div class="box">
+      <h3>Contact info</h3>
+      <a href="./home.php"><i class="fas fa-phone"></i>+94 112 699827</a>
+      <a href="./completed.php"><i class="fas fa-envelope"></i>info@webuild.lk</a>
+      <a href="./about.php"><i class="fas fa-angle-right"></i>About Us</a>
+      <a href="./contact.php"><i class="fas fa-map"></i>Reid Avenue ,Colombo7</a>
+    </div>
+  </div>
+  <div class="credit">WEBUILD (Pvt)Ltd</div>
+</section>
+
+
+
+
+
+
+
+<!-- footer section end -->
+
+
+  
+<!-- custon js file link -->
+<script src="./home.js"></script>
+
+</body>
+</html>
