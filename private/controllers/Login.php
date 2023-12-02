2c7d155ec49d32270a2fc3063b9bb544b3db50c6
GaveeshaWickrama <gaveesha.wickrama2001@gmail.com>
2023-12-03 00:54:32 +0530

    Update Login.php
    Added validation part

--- private/controllers/Login.php
+++ private/controllers/Login.php
@@ -4,7 +4,29 @@
     class Login extends Controller{
         
         public function index(){
-            $this->view('login');
+
+            //code
+            $errors = array();
+
+            if (count($_POST)>0){
+
+                $user=new Users();
+                if($row=$user->where('username',$_POST['email'])){
+
+                    Auth::authenticate($row);
+                    $this->reidrect('/home');
+                }else{
+                    $errors['username']="Wrong username or password";
+
+
+
+
+                }
+            }
+
+            $this->view('login',[
+                'errors'=>$errors,
+            ]);
         }
 
     }
