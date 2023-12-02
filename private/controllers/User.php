9cd2a96c01c49c500765daa0ae12656e5ea59beb
GaveeshaWickrama <gaveesha.wickrama2001@gmail.com>
2023-12-03 03:54:19 +0530

    user.view created

---
+++ private/controllers/User.php
@@ -0,0 +1,33 @@
+<?php
+
+//user controller
+    class User extends Controller{
+
+        public function index(){
+
+            if(!Auth::logged_in()){
+                $this->redirect('/login');
+            }
+
+            $users=new Users();
+            
+            //$arr=array ( "First_Name" => "Gaveesha", "Last_Name" => "Wick","NIC" =>"2999999" ,"Contact_Number"=> "071-9907865" ,"Address" => "blah","Username" => "nm","Email_Address"=>"mn@","Password" =>"hg","Confirm_Password" => "hg" );
+            //$arr=array ( "First_Name" => "Gavee" );
+            //$users->insert($arr);
+            //$users->update(2,$arr);
+           // $users->delete(1);
+
+            $data=$users->findAll();
+
+            $this->view('user',['rows'=> $data]);
+
+
+
+
+            
+            $this->view('user'.['rows'=>$data]);
+
+        }
+
+    }
+?>
--- private/views/user.view.php
+++ private/views/user.view.php
@@ -1,10 +1,19 @@
 <?php $this->view('includes/header')?>
 
 
-     <?php
-          echo "<pre>";
-          print_r($rows);
-     ?>
+     <!-- <?php
+          //echo "<pre>";
+          //print_r($rows);
+     ?> -->
+
+
+     <?php foreach ($rows as $row) :?>
+          <h5> <?=$row->firstname?> <?=$row->lastname?> </h5>
+          <p><?=$row->contactnumber?></p>
+          <a href="">Profile</a>
+          <br>
+          <br>
+     <?php endforeach;?>
      
 
 
