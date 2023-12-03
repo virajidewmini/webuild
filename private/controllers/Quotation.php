ec2aa717c0b41378c326c1bade849c902ee65539
GaveeshaWickrama <gaveesha.wickrama2001@gmail.com>
2023-12-03 09:08:37 +0530

    created quotation controller and view

---
+++ private/controllers/Quotation.php
@@ -0,0 +1,11 @@
+<?php
+
+    //Quotation controller
+    class Quotation extends Controller{
+        
+        public function index(){
+            $this->view('quotation');
+        }
+
+    }
+?>
