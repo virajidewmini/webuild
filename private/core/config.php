d08237cc5aa142262493e9ec304c11808047476d
GaveeshaWickrama <gaveesha.wickrama2001@gmail.com>
2023-12-05 22:55:28 +0530

    config file

--- private/core/config.php
+++ private/core/config.php
@@ -1,8 +1,6 @@
 <?php
     define ('ROOT','http://localhost/webuild/public');
     define('SITENAME', 'webuild');
-
-
     define('DBNAME', 'webuild');
     define('DBHOST', 'localhost');
     define('DBUSER', 'root');
