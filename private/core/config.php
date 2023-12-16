ca0a2a16c7c874095fac7804c2e2c77a28ee84cb
Thenuwara <pramukhapoo@gmail.com>
2023-12-16 22:07:59 +0530

    added staff login

--- private/core/config.php
+++ private/core/config.php
@@ -2,7 +2,7 @@
     define ('ROOT','http://localhost/webuild/public');
     define('SITENAME', 'webuild');
     define('DBNAME', 'webuild');
-    define('DBHOST', 'localhost');
+    define('DBHOST', 'localhost:3308');
     define('DBUSER', 'root');
     define('DBPASS', '');
     define('DBDRIVER', 'mysql');
