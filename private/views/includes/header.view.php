1725a735626fd14e51e1aca4228383f10d6bc431
GaveeshaWickrama <gaveesha.wickrama2001@gmail.com>
2023-12-18 15:39:58 +0530

    nav bar fixed

--- private/views/includes/header.view.php
+++ private/views/includes/header.view.php
@@ -153,6 +153,271 @@
                     <span class="text">Images</span>
                   </a>
                 </li>
+                <!-- settings and logout -->
+
+                <ul class="side-menu">
+                  <li>
+                    <a href="#">
+                      <i class="fas fa-cog"></i>
+                      <span class="text">Settings</span>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="<?=ROOT?>/logout" class="logout">
+                      <i class="fas fa-right-from-bracket"></i>
+                      <span class="text" >Logout</span>
+                    </a>
+                  </li>
+                </ul>
+              </section>
+
+              <section class="content">
+                <nav>
+                  <i class="fas fa-bars menu-btn"></i>
+                  <form action="#">
+                    <div class="form-input">
+                      <input type="search" placeholder="search..." />
+                      <button class="search-btn">
+                        <i class="fas fa-search search-icon"></i>
+                      </button>
+                    </div>
+                  </form>
+
+                  <a href="#" class="notification">
+                    <i class="fas fa-bell"></i>
+                    <span class="num">28</span>
+                  </a>
+
+                  <a href="<?=ROOT?>/Staffprofile" class="profile">
+                    <img src="<?=ROOT?>/img/profile.png" alt="" />
+                    <!-- USER -->
+                    <?=Auth::getFirstname()?>
+                  </a>
+                </nav>
+              <main>
+            <?php elseif(Auth::getRole()== 'supervisor'): ?>
+              <ul class="side-menu top">
+                <li class="active">
+                  <a href="#" class="nav-link">
+                    <i class="fas fa-border-all"></i>
+                    <span class="text">super Dashboard</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="#" class="nav-link">
+                    <i class="fas fa-file"></i>
+                    <span class="text">Report</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="#" class="nav-link">
+                    <i class="fas fa-chart-simple"></i>
+                    <span class="text">Progress</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="#" class="nav-link">
+                    <i class="fa-brands fa-paypal"></i>
+                    <span class="text">Payment</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="#" class="nav-link">
+                    <i class="fas fa-comments"></i>
+                    <span class="text">Complaint</span>
+                  </a>
+                </li>
+                <!-- settings and logout -->
+
+                <ul class="side-menu">
+                  <li>
+                    <a href="#">
+                      <i class="fas fa-cog"></i>
+                      <span class="text">Settings</span>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="<?=ROOT?>/logout" class="logout">
+                      <i class="fas fa-right-from-bracket"></i>
+                      <span class="text" >Logout</span>
+                    </a>
+                  </li>
+                </ul>
+              </section>
+
+              <section class="content">
+                <nav>
+                  <i class="fas fa-bars menu-btn"></i>
+                  <form action="#">
+                    <div class="form-input">
+                      <input type="search" placeholder="search..." />
+                      <button class="search-btn">
+                        <i class="fas fa-search search-icon"></i>
+                      </button>
+                    </div>
+                  </form>
+
+                  <a href="#" class="notification">
+                    <i class="fas fa-bell"></i>
+                    <span class="num">28</span>
+                  </a>
+
+                  <a href="<?=ROOT?>/Staffprofile" class="profile">
+                    <img src="<?=ROOT?>/img/profile.png" alt="" />
+                    <!-- USER -->
+                    <?=Auth::getFirstname()?>
+                  </a>
+                </nav>
+              <main>
+
+            <?php elseif(Auth::getRole()== 'storekeeper'): ?>
+              <ul class="side-menu top">
+                <li class="active">
+                  <a href="#" class="nav-link">
+                    <i class="fas fa-border-all"></i>
+                    <span class="text">store Dashboard</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="#" class="nav-link">
+                    <i class="fas fa-file"></i>
+                    <span class="text">Report</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="#" class="nav-link">
+                    <i class="fas fa-chart-simple"></i>
+                    <span class="text">Progress</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="#" class="nav-link">
+                    <i class="fa-brands fa-paypal"></i>
+                    <span class="text">Payment</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="#" class="nav-link">
+                    <i class="fas fa-comments"></i>
+                    <span class="text">Complaint</span>
+                  </a>
+                </li>
+                <!-- settings and logout -->
+
+                <ul class="side-menu">
+                  <li>
+                    <a href="#">
+                      <i class="fas fa-cog"></i>
+                      <span class="text">Settings</span>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="<?=ROOT?>/logout" class="logout">
+                      <i class="fas fa-right-from-bracket"></i>
+                      <span class="text" >Logout</span>
+                    </a>
+                  </li>
+                </ul>
+              </section>
+
+              <section class="content">
+                <nav>
+                  <i class="fas fa-bars menu-btn"></i>
+                  <form action="#">
+                    <div class="form-input">
+                      <input type="search" placeholder="search..." />
+                      <button class="search-btn">
+                        <i class="fas fa-search search-icon"></i>
+                      </button>
+                    </div>
+                  </form>
+
+                  <a href="#" class="notification">
+                    <i class="fas fa-bell"></i>
+                    <span class="num">28</span>
+                  </a>
+
+                  <a href="<?=ROOT?>/Staffprofile" class="profile">
+                    <img src="<?=ROOT?>/img/profile.png" alt="" />
+                    <!-- USER -->
+                    <?=Auth::getFirstname()?>
+                  </a>
+                </nav>
+              <main>
+            <?php elseif(Auth::getRole()== 'admin'): ?>
+              <ul class="side-menu top">
+                <li class="active">
+                  <a href="#" class="nav-link">
+                    <i class="fas fa-border-all"></i>
+                    <span class="text">admin Dashboard</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="#" class="nav-link">
+                    <i class="fas fa-file"></i>
+                    <span class="text">Report</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="#" class="nav-link">
+                    <i class="fas fa-chart-simple"></i>
+                    <span class="text">Progress</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="#" class="nav-link">
+                    <i class="fa-brands fa-paypal"></i>
+                    <span class="text">Payment</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="#" class="nav-link">
+                    <i class="fas fa-comments"></i>
+                    <span class="text">Complaint</span>
+                  </a>
+                </li>
+                <!-- settings and logout -->
+
+                <ul class="side-menu">
+                  <li>
+                    <a href="#">
+                      <i class="fas fa-cog"></i>
+                      <span class="text">Settings</span>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="<?=ROOT?>/logout" class="logout">
+                      <i class="fas fa-right-from-bracket"></i>
+                      <span class="text" >Logout</span>
+                    </a>
+                  </li>
+                </ul>
+              </section>
+
+              <section class="content">
+                <nav>
+                  <i class="fas fa-bars menu-btn"></i>
+                  <form action="#">
+                    <div class="form-input">
+                      <input type="search" placeholder="search..." />
+                      <button class="search-btn">
+                        <i class="fas fa-search search-icon"></i>
+                      </button>
+                    </div>
+                  </form>
+
+                  <a href="#" class="notification">
+                    <i class="fas fa-bell"></i>
+                    <span class="num">28</span>
+                  </a>
+
+                  <a href="<?=ROOT?>/Staffprofile" class="profile">
+                    <img src="<?=ROOT?>/img/profile.png" alt="" />
+                    <!-- USER -->
+                    <?=Auth::getFirstname()?>
+                  </a>
+                </nav>
+              <main>
             <?php else: ?>
               <ul class="side-menu top">
                 <li class="active">
