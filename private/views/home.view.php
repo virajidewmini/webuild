63af283a17280516b5246568d8e93eedd80daecc
HimashaRandika <136648121+HimashaRandika@users.noreply.github.com>
2023-12-22 12:33:46 +0530

    Update home

--- private/views/home.view.php
+++ private/views/home.view.php
@@ -3,11 +3,16 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Home</title>
+  <title>Houses</title>
 
+  <!-- swiper cs link -->
+  <link
+  rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
+  <script src="https://kit.fontawesome.com/9e9a03ae37.js" crossorigin="anonymous"></script>
+  
   <!-- font awesome cdn link -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
-  <link rel="stylesheet" href="./public/css/home.css">
+  <link rel="stylesheet" href="./style.css">
 </head>
 <body>
 
@@ -19,14 +24,17 @@
         <img src="./Photos/LOGO2.png" alt="" class="LOGO-img" width="50" hight="50">
 </a> -->
 
-<a href="./home.php" class="logo">WEBUILD</a>
+<a href="./houses.php" class="logo">WEBUILD</a>
 <nav class="navbar">
-  <a href="./home.php">Home</a>
+  <a href="./lands.php">Lands</a>
+  <a href="./houses.php">House</a>
   <a href="./completed.php">Completed Projects</a>
   <a href="./about.php">About Us</a>
   <a href="./contact.php">Contact Us</a>
-
 </nav>
+
+
+
 
 <div id="menu-btn" class="fas fa-bars"></div>
 
@@ -34,12 +42,47 @@
 
 <!-- header section ends -->
 
+<!-- home section starts -->
+<section class="home">
+  <div class="swiper home-slider">
+    <div class="swiper-wrapper">
+      <div class="swiper-slide slide " style="background: url(./Photos/741_house_1.jpg);">
+        <div class="content">
+          <span>purchase your dream place</span>
+          <h3>dream with webuild</h3>
+          <a href="#" class="btn">See More</a>       
+         </div>
+      </div>
+      <div class="swiper-slide slide " style="background: url(./Photos/1630000306A.jpg);">
+        <div class="content">
+          <span>purchase your dream place</span>
+          <h3>dream with webuild</h3>
+          <a href="#" class="btn">See More</a>        
+        </div>
+      </div>
+      <div class="swiper-slide slide " style="background: url(./Photos/hq720.jpg)">
+        <div class="content">
+          <span>purchase your dream place</span>
+          <h3>dream with webuild</h3>
+          <a href="#" class="btn">See More</a>
+        </div>
+      </div>
+    </div>
+
+    <div class="swiper-button-next"></div>
+    <div class="swiper-button-prev"></div>
+
+  </div>
+</section>
+<!-- home section starts -->
+
+
 <!-- service section  starts -->
 <section class="services">
   <h1 class="heading-title">Our Houses</h1>
   <div class="box-container">
-    <div class="box1">
-      <img src="./Photos/download 1.jpeg" alt="">
+    <!-- <div class="box1">
+      <img src="images/icon-1.png" alt="">
     </div>
     <div class="box2">
       <img src="./Photos/images.jpeg" alt="">
@@ -49,17 +92,231 @@
     </div>
     <div class="box4">
       <img src="./Photos/00.jpg" alt="">
-    </div>
+    </div> -->
   </div>
 </section>
 <!-- service section ends -->
 
+<!-- filter section starts-->
+<div class="slide" style="background: url(.../Photos/hq720.jpg)">
+        <div class="content">
+        <section class="booking">
+  <h1 class="heading-title"> Contact Us
+  <form action="book_form.php" method="post" class="book-form">
+    <div class="flex">
+      <div class="inputBox">
+        <span>Houses Models</span>
+        <select name="models" id="houses">
+          <option value="">select house categary</option>
+          <option value="1 floor">1 floor</option>
+          <option value="2 floor">2 floor</option>
+          <option value="3 floor">3 floor</option>
+        </select>
+      </div>
+      <div class="inputBox">
+        <span>District:</span>
+        <input type="text" placeholder="All District" name="email">
+        <select name="models" id="houses">
+          <option value="">select district</option>
+          <option value="Galle">Galle</option>
+          <option value="Colombo">Colombo</option>
+          <option value="Gampaha">Gampaha</option>
+          <option value="Kaluthara">Kaluthara</option>
+          <option value="Matara">Matara</option>
+          <option value="Kandy">Kandy</option>
+          <option value="Nuwaraeliya">Nuwaraeliya</option>
+        </select>
+      </div>
+      <div class="inputBox">
+        <span>popular ciites:</span>
+        <input type="text" placeholder="Cities" name="phone">
+        <select name="models" id="houses">
+          <option value="">select Town</option>
+          <option value="Galle">Galle</option>
+          <option value="Ambalangoda">Ambalangoda</option>
+          <option value="Battaramulla">Battaramulla</option>
+          <option value="Bambalapitiya">Bambalapitiya</option>
+          <option value="Nugegoda">Nugegoda</option>
+          <option value="Kaluthara">Kaluthara</option>
+          <option value="Mathugama">Mathugama</option>
+          <option value="Panadura">Panadura</option>
+          <option value="Kandy">Kandy</option>
+          <option value="Nuwaraeliya">Nuwaraeliya</option>
+        </select>
+      </div>
+      <div class="inputBox">
+        <span>search:</span>
+        <input type="text" placeholder="Type here" name="address">
+      </div>
+    </div>
+    <input type="submit" value="submit" class="btn" name="send">
+  </form>  
+  </h1>
+</section>
+        </div>
+      </div>
 
-     <h1>Hello Home</h1>
-     <?php
-          print_r($rows);
-     ?>
-     
+
+
+
+
+
+
+
+<!-- filter section ends-->
+
+<!-- district cities section starts -->
+<section class="footer">
+  <div class="box-container">
+    <div class="box">
+      <h3>DISTRICTS</h3>
+      <a href="./home.php"><i class="fas fa-angle-right"></i>Colombo</a>
+      <a href="./completed.php"><i class="fas fa-angle-right"></i>Galle</a>
+      <a href="./about.php"><i class="fas fa-angle-right"></i>Kaluthara</a>
+      <a href="./contact.php"><i class="fas fa-angle-right"></i>Matara</a>
+      <a href="./contact.php"><i class="fas fa-angle-right"></i>Gampaha</a>
+      <a href="./contact.php"><i class="fas fa-angle-right"></i>Hambanthota</a>
+      <a href="./contact.php"><i class="fas fa-angle-right"></i>Kandy</a>
+    </div>
+
+    <div class="box">
+      <h3></h3>
+      <a href="./about.php"><i class="fas fa-angle-right"></i>Kandy</a>
+      <a href="./contact.php"><i class="fas fa-angle-right"></i>Rathnapuara</a>
+    </div>
+
+    <div class="box">
+      <h3>Popular Cities</h3>
+      <a href="./home.php"><i class="fas fa-angle-right"></i>Baththaramulla</a>
+      <a href="./completed.php"><i class="fas fa-angle-right"></i>Rajagiriya</a>
+      <a href="./contact.php"><i class="fas fa-angle-right"></i>Galle</a>
+      <a href="./contact.php"><i class="fas fa-angle-right"></i>Dodangoda</a>
+      <a href="./contact.php"><i class="fas fa-angle-right"></i>Ambalangoda</a>
+      <a href="./contact.php"><i class="fas fa-angle-right"></i>Beruwala</a>
+      <a href="./contact.php"><i class="fas fa-angle-right"></i>Tangalle</a>
+      <a href="./contact.php"><i class="fas fa-angle-right"></i>Kaluthara</a>
+      <a href="./contact.php"><i class="fas fa-angle-right"></i>Weerakatiya</a>
+    </div>
+  </div>
+</section>
+<!-- district cities section ends -->
+
+
+
+
+
+
+
+<!-- home packages section starts  -->
+<section class="home-packages">
+  <h1 class="heading-title">Our packages</h1>
+  <div class="box-container">
+    <div class="box">
+      <div class="image">
+        <img src="./Photos/images5.jpeg" alt="">
+      </div>
+      <div class="content">
+        <h3>Purch Your Lands</h3>
+        <p>jdnjndnnnn jmkkmk okmlkmmf kmmm</p>
+        <a href="#" class="btn">Visit</a>
+      </div>
+    </div>
+    <div class="box">
+      <div class="image">
+        <img src="./Photos/images5.jpeg" alt="">
+      </div>
+      <div class="content">
+        <h3>Purch Your Lands</h3>
+        <p>jdnjndnnnn jmkkmk okmlkmmf kmmm</p>
+        <a href="#" class="btn">Visit</a>
+      </div>
+    </div>
+    <div class="box">
+      <div class="image">
+        <img src="./Photos/images5.jpeg" alt="">
+      </div>
+      <div class="content">
+        <h3>Purch Your Lands</h3>
+        <p>jdnjndnnnn jmkkmk okmlkmmf kmmm</p>
+        <a href="#" class="btn">Visit</a>
+      </div>
+    </div>
+  </div>
+  <div class="load-more"><a href="#" class="btn">Load more</a></div>
+</section>
+<!-- home packages section ends  -->
+
+<!-- home offer section starts -->
+<section class="home-offer">
+  <div class="content">
+    <h3>upto 3% off</h3>
+    <p>jijojojioj jjkikjd jijijd ffrw l,lw wtftfwd ftfw rcf tft tftw guhj   </p>
+    <a href="#" class="btn">book now</a>
+  </div>
+</section>
+<!-- home offer section ends -->
+
+<!-- reviews section starts -->
+<section class="reviews">
+  <div class="reviews-slider">
+    <div class="w">
+      <div class="slide">
+        <div class="stars">
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+        </div>
+        <p>lorem huh huhuhs sjois ijiia ijis iji ijiw ijiw ijiw ijijw ijiw ijiw jiw oow</p>
+        <h3>N.M.Siriwardhane</h3>
+        <span>Doctor</span>
+      </div>
+      <div class="slide">
+        <div class="stars">
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+        </div>
+        <p>lorem huh huhuhs sjois ijiia ijis iji ijiw ijiw ijiw ijijw ijiw ijiw jiw oow</p>
+        <h3>N.M.Siriwardhane</h3>
+        <span>Doctor</span>
+      </div>
+      <div class="slide">
+        <div class="stars">
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+        </div>
+        <p>lorem huh huhuhs sjois ijiia ijis iji ijiw ijiw ijiw ijijw ijiw ijiw jiw oow</p>
+        <h3>N.M.Siriwardhane</h3>
+        <span>Doctor</span>
+      </div>
+      <div class="slide">
+        <div class="stars">
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+        </div>
+        <p>lorem huh huhuhs sjois ijiia ijis iji ijiw ijiw ijiw ijijw ijiw ijiw jiw oow</p>
+        <h3>N.M.Siriwardhane</h3>
+        <span>Doctor</span>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- reviews section ends -->
+
+
+
+
+
 
 
 
@@ -99,7 +356,9 @@
 
 
 <!-- footer section end -->
+<!-- swiper link js -->
 
+<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
 
   
 <!-- custon js file link -->
