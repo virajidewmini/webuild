5ca2e94258a0b835df03c1312232356102b43dff
Thenuwara <pramukhapoo@gmail.com>
2024-01-06 13:11:20 +0530

    added modification billing part

--- private/controllers/Pmmodification.php
+++ private/controllers/Pmmodification.php
@@ -3,8 +3,27 @@
     //Manager materials controller
     class Pmmodification extends Controller{
         
-        public function index(){
-            $this->view('pmmodification');
+
+        public function index()
+        {
+            if(!Auth::logged_in()){
+                $this->redirect('/login');
+            }
+            $pmi = Auth::getid();
+
+            $modification = new Modification();
+            $data = $modification->where('user_id',2);
+
+            $def = new Def();
+            $data1 = $def->where('id',1);
+
+
+
+            $this->view('pmmodification',[
+                'row'=>$data,
+                'row1'=>$data1,
+            ]);
+        
         }
 
     }
