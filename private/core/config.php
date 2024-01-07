6da66cb30d56554acb4a24034579dff754981e6c
HimashaRandika <136648121+HimashaRandika@users.noreply.github.com>
2024-01-07 22:46:50 +0530

    config update

--- private/core/config.php
+++ private/core/config.php
@@ -2,15 +2,9 @@
     define ('ROOT','http://localhost/webuild/public');
     define('SITENAME', 'webuild');
     define('DBNAME', 'webuild');
-    define('DBHOST', 'localhost:3308');
-    define('DBUSER', 'root');
-    define('DBPASS', '');
-    define('DBDRIVER', 'mysql');
-
-    define('DBNAME', 'webuild');
     define('DBHOST', 'localhost');
     define('DBUSER', 'root');
     define('DBPASS', '');
-    define('DBDRIVER','mysql');
+    define('DBDRIVER', 'mysql');
 
 ?>
