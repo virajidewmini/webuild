67989532b367d087ddb073c1089aa962231955b0
Viraji Dewmini <virajidewmini@gmail.com>
2024-02-10 08:34:54 +0530

    integrate payment gateway

--- private/views/includes/footer.view.php
+++ private/views/includes/footer.view.php
@@ -6,5 +6,8 @@
     <script src="<?=ROOT?>/js/SubmitModel.js"></script>
     <script src="<?=ROOT?>/js/AllocateCoworkers.js"></script>
     <script src="<?=ROOT?>/js/AddCoworker.js"></script>
+    <script src="<?=ROOT?>/js/Payment.js"></script>
+    <script type="text/javascript" src="https://www.payhere.lk/lib/payhere.js"></script>
+
   </body>
 </html>
