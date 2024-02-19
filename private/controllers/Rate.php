e51ef30e05d18ca7313280265fe4570aa85e292e
Viraji Dewmini <virajidewmini@gmail.com>
2024-02-19 09:56:32 +0530

    View of rating and reviews

---
+++ private/controllers/Rate.php
@@ -0,0 +1,10 @@
+<?php
+
+    class Rate extends Controller{
+        
+        public function index(){
+            $this->view('RateAndReview');
+        }
+
+    }
+?>
---
+++ private/views/RateAndReview.view.php
@@ -0,0 +1,140 @@
+<?php $this->view('includes/header')?>
+
+
+<div class="rate-heading">
+    <span>Comments</span>
+    <h1>Clients Says</h1>
+  </div>
+  <div class="rate-container">
+    <div class="rate-box">
+      <div class="rate-top">
+        <div class="rate-profile">
+          <div class="rate-profile-img">
+            <img src="<?=ROOT?>/img/profile.jpg">
+          </div>
+          <div class="name-user">
+            <strong>Viraji Dewmini</strong>
+            <span>virajidewmini@gmail.Com</span>
+          </div>
+        </div>
+        <div class="reviews">
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="far fa-star"></i>
+        </div>
+      </div>
+
+      <div class="rate-comment">
+        <p>User-friendly platform that effectively showcases the company's services and portfolio. The modern design and high-quality imagery create a professional impression. Navigation is intuitive, but could be improved with clearer labeling in some sections. Contact information is easily accessible, though adding a contact form would streamline communication. The website loads quickly and is responsive across devices, ensuring a seamless browsing experience. Overall, it's a valuable resource for potential clients seeking construction services.</p>
+      </div>
+    </div>
+
+    <div class="rate-box">
+      <div class="rate-top">
+        <div class="rate-profile">
+          <div class="rate-profile-img">
+            <img src="<?=ROOT?>/img/profile.jpg">
+          </div>
+          <div class="name-user">
+            <strong>Viraji Dewmini</strong>
+            <span>virajidewmini@gmail.Com</span>
+          </div>
+        </div>
+        <div class="reviews">
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="far fa-star"></i>
+        </div>
+      </div>
+
+      <div class="rate-comment">
+        <p>User-friendly platform that effectively showcases the company's services and portfolio. The modern design and high-quality imagery create a professional impression. Navigation is intuitive, but could be improved with clearer labeling in some sections. Contact information is easily accessible, though adding a contact form would streamline communication. The website loads quickly and is responsive across devices, ensuring a seamless browsing experience. Overall, it's a valuable resource for potential clients seeking construction services.</p>
+      </div>
+    </div>
+
+    <div class="rate-box">
+      <div class="rate-top">
+        <div class="rate-profile">
+          <div class="rate-profile-img">
+            <img src="<?=ROOT?>/img/profile.jpg">
+          </div>
+          <div class="name-user">
+            <strong>Viraji Dewmini</strong>
+            <span>virajidewmini@gmail.Com</span>
+          </div>
+        </div>
+        <div class="reviews">
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="far fa-star"></i>
+        </div>
+      </div>
+
+      <div class="rate-comment">
+        <p>User-friendly platform that effectively showcases the company's services and portfolio. The modern design and high-quality imagery create a professional impression. Navigation is intuitive, but could be improved with clearer labeling in some sections. Contact information is easily accessible, though adding a contact form would streamline communication. The website loads quickly and is responsive across devices, ensuring a seamless browsing experience. Overall, it's a valuable resource for potential clients seeking construction services.</p>
+      </div>
+    </div>
+
+    <div class="rate-box">
+      <div class="rate-top">
+        <div class="rate-profile">
+          <div class="rate-profile-img">
+            <img src="<?=ROOT?>/img/profile.jpg">
+          </div>
+          <div class="name-user">
+            <strong>Viraji Dewmini</strong>
+            <span>virajidewmini@gmail.Com</span>
+          </div>
+        </div>
+        <div class="reviews">
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="far fa-star"></i>
+        </div>
+      </div>
+
+      <div class="rate-comment">
+        <p>User-friendly platform that effectively showcases the company's services and portfolio. The modern design and high-quality imagery create a professional impression. Navigation is intuitive, but could be improved with clearer labeling in some sections. Contact information is easily accessible, though adding a contact form would streamline communication. The website loads quickly and is responsive across devices, ensuring a seamless browsing experience. Overall, it's a valuable resource for potential clients seeking construction services.</p>
+      </div>
+    </div>
+  </div>
+
+
+  <!-- here is star rating -->
+
+  <div class="main-container">
+    <div class="star-container">
+     <h1>Put Your Reviews & Ratings</h1>
+      <div class="star-widget">
+        <input type="radio" name="rate" id="rate-5" style="display:none;">
+        <label for="rate-5" class="fas fa-star"></label>
+        <input type="radio" name="rate" id="rate-4" style="display:none;">
+        <label for="rate-4" class="fas fa-star"></label>
+        <input type="radio" name="rate" id="rate-3" style="display:none;">
+        <label for="rate-3" class="fas fa-star"></label>
+        <input type="radio" name="rate" id="rate-2" style="display:none;">
+        <label for="rate-2" class="fas fa-star"></label>
+        <input type="radio" name="rate" id="rate-1" style="display:none;">
+        <label for="rate-1" class="fas fa-star"></label>
+        <form action="#">
+          <header></header>
+          <div>
+            <input type="text" class="v_form-control" placeholder="Describe your experience.."></input>
+          </div>
+          <div>
+            <button class="v_submit_button" style="width:325px" type="submit">Post</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+
+ 
