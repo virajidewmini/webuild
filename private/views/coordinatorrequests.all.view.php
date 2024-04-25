cc4be2c3354a40d6e7db9dd4b561952a64bc00bb
GaveeshaWickrama <gaveesha.wickrama2001@gmail.com>
2024-04-25 11:35:05 +0530

    storekeeper changes

--- private/views/coordinatorrequests.all.view.php
+++ private/views/coordinatorrequests.all.view.php
@@ -318,7 +318,7 @@
 </div>
 </div>
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.9.3/html2pdf.bundle.min.js"></script>
 
         <script>
