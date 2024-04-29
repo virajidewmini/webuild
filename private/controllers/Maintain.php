731ca86d3ca9f81de61316553f7e8d5ad346debc
HimashaRandika <136648121+HimashaRandika@users.noreply.github.com>
2024-04-29 11:54:32 +0530

    filter and name update

--- private/controllers/Maintain.php
+++ private/controllers/Maintain.php
@@ -10,6 +10,18 @@ class Maintain extends Controller
     $paints = $storeMaintain->getPaintsdetails();
  
 
+    $filter = isset($_GET['material_name']) ? $_GET['material_name'] : null;
+
+        // If a filter is applied, filter the data by material_name
+        if ($filter !== null) {
+            // Perform filtering based on material_name
+            $filteredData = array_filter($data, function ($row) use ($filter) {
+                return stripos($row->material_name, $filter) !== false;
+            });
+
+            $data = $filteredData;
+        }
+
     $this->view('storekeeperMaintain',[
         'rows'=>$data,
         'tiles'=>$tiles,
