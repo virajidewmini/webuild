e772316354e5cd8c77552e914c82fdd79e261233
GaveeshaWickrama <gaveesha.wickrama2001@gmail.com>
2024-04-30 11:36:33 +0530

    Update Pmmaterial.php

--- private/controllers/Pmmaterial.php
+++ private/controllers/Pmmaterial.php
@@ -7,7 +7,7 @@ class Pmmaterial extends Controller
     public function index()
     {
         if (!Auth::logged_in()) {
-            $this->redirect('login');
+            $this->redirect('staff_login');
         }
         $pmid = Auth::getId();
         $projects = new Projects();
@@ -43,53 +43,6 @@ class Pmmaterial extends Controller
         ]);
     }
 
-    public function materialRequest_view($request_id = null)
-    {
-            if(!Auth::logged_in()){
-                $this->redirect('login');
-            }
-            $material_req= new Material_requests();
-
-            $row = $material_req->getReqDetails($request_id);
-
-
-            $this->view('pmmaterial_r_details',[
-                'rows'=>$row,
-            ]);
-    }
-    public function materialReceive_view($quotation_id = null, $project_id = null, $level = null )
-    {
-            if(!Auth::logged_in()){
-                $this->redirect('login');
-            }
-            $material_quo= new QuotationSK();
-
-            $row = $material_quo->getReceiveReqDetails($quotation_id);
-
-
-            $this->view('pmmaterial_q_details',[
-                'rows'=>$row,
-                'project_id'=>$project_id,
-                'level'=>$level,
-            ]);
-    }
-
-    public function materialReceived_view($quotation_id = null, $project_id = null, $level = null )
-    {
-            if(!Auth::logged_in()){
-                $this->redirect('login');
-            }
-            $material_quo= new QuotationSK();
-
-            $row = $material_quo->getReceiveReqDetails($quotation_id);
-
-
-            $this->view('pmmaterial_received_details',[
-                'rows'=>$row,
-                'project_id'=>$project_id,
-                'level'=>$level,
-            ]);
-    }
     public function materialReceived($id = null)
     {
             if(!Auth::logged_in()){
@@ -97,7 +50,5 @@ class Pmmaterial extends Controller
             }
             $q_table = new QuotationSK();
             $q_table->materialReceived($id);
-
-            echo '<script>window.history.go(-1);</script>';
     }
 }
