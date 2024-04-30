2ad1d5576b3607473a494bf309f9833caeb0e19d
GaveeshaWickrama <118794503+GaveeshaWickrama@users.noreply.github.com>
2024-04-30 11:34:44 +0530

    Update config.php

--- private/core/config.php
+++ private/core/config.php
@@ -2,7 +2,7 @@
     define ('ROOT','http://localhost/webuild/public');
     define('SITENAME', 'webuild');
     define('DBNAME', 'webuild');
-    define('DBHOST', 'localhost:3308');
+    define('DBHOST', 'localhost');
     define('DBUSER', 'root');
     define('DBPASS', '');
     define('DBDRIVER', 'mysql');
